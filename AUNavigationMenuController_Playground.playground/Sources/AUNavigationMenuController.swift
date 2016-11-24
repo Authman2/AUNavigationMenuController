9b65848a1d28c611dc694a26d24ffbc84cc3ae10
Adeola Uthman <authman2@gmail.com>
2016-11-23 19:06:32 -0500

    Fixed issue with cover menu
    TODO:
    - Add collection view to menu view

--- AUNavigationMenuController_Playground.playground/Sources/AUNavigationMenuController.swift
+++ AUNavigationMenuController_Playground.playground/Sources/AUNavigationMenuController.swift
@@ -10,26 +10,32 @@ public class AUNavigationMenuController: UINavigationController {
     public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil);
         setupTapGesture();
+        // Creates a menu view and adds it behind everything.
+        addMenuView();
     }
     
     public override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController);
         setupTapGesture();
+        // Creates a menu view and adds it behind everything.
+        addMenuView();
     }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
     public override func viewDidLoad() {
         super.viewDidLoad();
         setupTapGesture();
+        // Creates a menu view and adds it behind everything.
+        addMenuView();
     }
-    
     
     public override func didMove(toParentViewController parent: UIViewController?) {
         setupTapGesture();
+        // Creates a menu view and adds it behind everything.
+        addMenuView();
     }
     
     
@@ -42,6 +48,10 @@ public class AUNavigationMenuController: UINavigationController {
     //  Variables
     //
     /////////////////////////
+    
+    // The actual menu
+    private let menuView: UIView = UIView();
+    
     
     // Whether or not the menu is open.
     public var open: Bool = Bool();
@@ -82,18 +92,24 @@ public class AUNavigationMenuController: UINavigationController {
     
     /* Opens a pull down style menu. */
     @objc private func openPulldownMenu() {
-        // Creates a menu view and adds it behind everything.
-        //addMenuView();
-        
         
         // Open
         if(!open) {
+            
+            // Set the height to 0 to prevent premature overlapping.
+            if(shouldCoverViewContents) {
+                self.menuView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 0);
+            }
+            
             UIView.animate(withDuration: 0.35, delay: 0.2, options: [], animations: {
                 
                 if(self.shouldCoverViewContents) {
                     self.navigationBar.frame.origin.y += self.pullAmount;
+                    self.view.bringSubview(toFront: self.menuView);
+                    self.changeMenuSize(grow: true);
                 } else {
                     self.view.frame.origin.y += self.pullAmount;
+                    self.menuView.frame.origin.y -= self.pullAmount;
                 }
                 
             }, completion: nil);
@@ -105,16 +121,28 @@ public class AUNavigationMenuController: UINavigationController {
             UIView.animate(withDuration: 0.35, delay: 0.2, options: [], animations: {
                 
                 if(self.shouldCoverViewContents) {
-                    self.navigationBar.frame.origin.y -= self.pullAmount;
+                    self.navigationBar.frame.origin.y -= self.pullAmount;                    self.changeMenuSize(grow: false);
                 } else {
                     self.view.frame.origin.y -= self.pullAmount;
+                    self.menuView.frame.origin.y += self.pullAmount;
                 }
                 
             }, completion: nil);
             open = false;
             
+            
         } // End of if-statement.
         
+    }
+    
+    
+    /* Adjusts the size of the menu over a period of time. */
+    private func changeMenuSize(grow: Bool) {
+        if(grow) {
+            self.menuView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: pullAmount);
+        } else {
+            self.menuView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 0);
+        }
     }
     
 
@@ -124,7 +152,16 @@ public class AUNavigationMenuController: UINavigationController {
     
     ////////// Adding Navigation Pages //////////
     
-    
+    private func addMenuView() {
+        // Initialize the menu view
+        menuView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: pullAmount);
+        menuView.backgroundColor = UIColor.green;
+        
+        
+        // Add the menu view to the container
+        view.addSubview(menuView);
+        view.sendSubview(toBack: menuView);
+    }
     
     
 }
